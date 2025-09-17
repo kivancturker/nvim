3c17452989b0425bd0e7dbc39a670e96443390b2
Kıvanç TÜRKER <kivanc.turker1997@gmail.com>
2025-09-17 17:54:04 +0300

    mason-lspconfig required_install becomes optional

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,7 +1,22 @@
 return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-      ensure_installed = { "lua_ls", "ts_ls", 'clangd' },
+      ensure_installed = { 
+        "lua_ls", 
+        -- "ts_ls", 
+        -- "bashls"
+        -- "clangd", 
+        -- "cssls",
+        -- "cmake-language-server",
+        -- "dockerls",
+        -- "mdx_analyzer",
+        -- "nginx-language-server",
+        -- "sqlls",
+        -- "tailwindcss",
+        -- "terraformls",
+        -- "rust_analyzer",
+        -- "html",
+      },
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
